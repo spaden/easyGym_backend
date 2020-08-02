56f516ded37d1ca370757920ad0da85128b0d5b9
Dheshoju Kalyan Kumar <kalyanspade.n@gmail.com>
2020-08-02 09:51:09 +0530

    backend started

---
+++ Routes/test.js
@@ -0,0 +1,8 @@
+let express = require('express'),
+    router = express.Router();
+
+router.get('/',(req,res)=> {
+    res.send("testing")
+})
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,31 @@
+let app = require('express')(),
+    server = require('http').Server(app),
+    bodyParser = require('body-parser')
+    express = require('express'),
+    cors = require('cors'),
+    http = require('http'),
+    path = require('path');
+
+let test = require('./Routes/test')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+    
+app.use(cors());
+app.use('/test', test)
+
+
+
+app.use(function(req, res, next) {
+    next();
+});
+
+/*first API to check if server is running*/
+app.get('*', (req, res) => {
+    res.send("working");
+})
+
+
+server.listen(3000, function() {
+    console.log('app listening on port: 3000');
+});
