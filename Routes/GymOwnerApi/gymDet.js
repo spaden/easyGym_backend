47194525af1390b237f4373cea7b4f7c5c19c1a1
Dheshoju Kalyan Kumar <kalyanspade.n@gmail.com>
2020-08-11 14:26:13 +0530

    Added confirmed Users

--- Routes/GymOwnerApi/gymDet.js
+++ Routes/GymOwnerApi/gymDet.js
@@ -21,9 +21,9 @@ router.post("/",(req,res)=> {
     })
 })
 
-router.post("/updateCurrentBookings",(req,res)=> {
+router.post("/deleteCurrentBookings",(req,res)=> {
     var dat = req.body
-    var sql = `update current_bookings_on_hold set status='${dat.status}' where id=${dat.id};`
+    var sql = `update current_bookings_on_hold set status='D' where id=${dat.id};`
     conn.query(sql,function(err,data,fields){
         if (err) {
             console.log(err)
@@ -37,5 +37,41 @@ router.post("/updateCurrentBookings",(req,res)=> {
 })
 
 
+
+
+
+router.post("/updateConfirmedUsers",(req,res)=> {
+    var dat = req.body
+    console.log(dat)
+    var sql =`start transaction;
+              update confirmedUsers set users='${JSON.stringify(dat.users)}' where gymId=${dat.gymId};
+              update current_bookings_on_hold set status='C' where id=${dat.id};
+              commit;`
+    conn.query(sql,function(err,data,fields){
+        if (err) {
+            console.log(err)
+            res.send(JSON.stringify({ result: "failed" }))
+        } else {
+            console.log("true 1")
+            res.send(JSON.stringify({ result: "passed"}))
+        }
+    })
+})
+
+
+router.post("/getConfirmedUsers",(req,res)=> {
+    var dat = req.body
+    var sql = `select * from confirmedUsers where gymId=${dat.gymId};`
+    conn.query(sql,function(err,data,fields){
+        if (err) {
+            console.log(err)
+            res.send(JSON.stringify({ result: "failed" }))
+        } else {
+            console.log("true 1")
+            res.send(JSON.stringify({ result: "passed", data: data}))
+        }
+    })
+
+})
 
 module.exports = router;
